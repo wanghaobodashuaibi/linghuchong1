ef2a1e89569aab2a4a91d28f27dc33e4da73197a
wanghaobo <wanghaobo2333@163.com>
2020-12-14 22:57:18 +0800

    Initial commit

---
+++ src/main/java/com/atguigu/linghuchong.java
@@ -0,0 +1,13 @@
+package com.atguigu;
+
+/**
+ * @description:
+ * @author: wanghaobo
+ * @time: 2020/12/14 22:55
+ */
+
+public class linghuchong {
+    public static void main(String[] args) {
+        System.out.println("111111111");
+    }
+}
